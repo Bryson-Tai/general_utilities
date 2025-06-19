6994116d1adc320d7e0526fe7ef120f355b38e37
Bryson Tai <brysontai1314@gmail.com>
2025-06-19 22:15:28 +0800

    Use key

--- opentofu/azure/kubernetes/outputs.tf
+++ opentofu/azure/kubernetes/outputs.tf
@@ -1,6 +1,6 @@
 output "aks" {
   value = {
-    for key, value in var.aks_configs : aks => {
+    for key, value in var.aks_configs : key => {
       id           = azurerm_kubernetes_cluster.aks[key].id
       outbound_ips = azurerm_kubernetes_cluster.aks[key].load_balancer_profile.effective_outbound_ips
     }
