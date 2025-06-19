c8831f7fbf8268c8cac2260d2c8b24e5efbda805
Bryson Tai <brysontai1314@gmail.com>
2025-06-19 22:20:28 +0800

    tfmt

--- opentofu/azure/kubernetes/outputs.tf
+++ opentofu/azure/kubernetes/outputs.tf
@@ -1,7 +1,7 @@
 output "aks" {
   value = {
     for key, value in var.aks_configs : key => {
-      id           = azurerm_kubernetes_cluster.aks[key].id
+      id = azurerm_kubernetes_cluster.aks[key].id
     }
   }
 }
