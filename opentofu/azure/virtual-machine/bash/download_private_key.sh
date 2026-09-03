eefc5f4ab00ca862583158bbbf6055ad6c1b8cd8
Bryson Tai <bryson.tai@mercedes-benz.com>
2026-09-04 00:58:33 +0800

    Add defer remove ssh

--- opentofu/azure/virtual-machine/bash/download_private_key.sh
+++ opentofu/azure/virtual-machine/bash/download_private_key.sh
@@ -5,6 +5,13 @@ fileName=azure_vm_personal_"${project_postfix}"
 
 mkdir -p "$sshDir"
 
-echo "${private_key}" > "$sshDir/$fileName"
+sshKeyPath="$sshDir/$fileName"
 
-chmod 600 "$sshDir/$fileName"
+
+if [ -n "$CREATE_PRIVATE_KEY" ]; then
+    echo "${private_key}" > "$sshKeyPath"
+
+    chmod 600 "$sshKeyPath"
+elif [ -n "$DELETE_PRIVATE_KEY" ]; then
+    rm -f "$sshKeyPath"
+fi
